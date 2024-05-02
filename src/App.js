a657b5b337b9145a25505a5a552192ce9df9c13d
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-02 18:42:08 +0530

    social media card design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Avatar from "./assets/images/avatar-jessica.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="social-media-card">
+        <div className="avatar">
+          <img src={Avatar} alt="avatar" />
+        </div>
+        <span className="name">Jessica Randall</span>
+        <span className="place">London, United Kingdom</span>
+        <p className="description">"Front-end developer and avid reader."</p>
+        <div className="button-section">
+          <button>GitHub</button>
+          <button> Frontend Mentor</button>
+          <button>LinkedIn</button>
+          <button>Twitter</button>
+          <button>Instagram</button>
+        </div>
+      </div>
     </div>
   );
 }
